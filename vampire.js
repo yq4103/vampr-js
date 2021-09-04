b1df7398b6eab98bd336fa66818eb4986029a2aa
Yirong Qin <yq4103@mun.ca>
2021-09-04 16:43:28 -0400

    vampire tree

--- vampire.js
+++ vampire.js
@@ -10,24 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
-
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -39,6 +48,19 @@ class Vampire {
 
   }
 }
+
+const original = new Vampire("original");
+const brat     = new Vampire("brat");
+const ansel    = new Vampire("ansel");
+const elgort   = new Vampire("elgort");
+const sarah    = new Vampire("sarah");
+const andrew  = new Vampire("andrew");
+
+original.addOffspring(ansel);
+original.addOffspring(brat);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
 
 module.exports = Vampire;
 
